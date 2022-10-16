439915a62e6da49fbb7e1651638e38e5cdf0ea0c
Adrian Ezequiel Gutierrez <adrian.gutierrezo@iteso.mx>
2022-10-16 18:21:21 -0500

    math.c and math.h

---
+++ math.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "math.h"
+
+int main(int argc, char** argv)
+{
+    return(EXIT_SUCCESS);   
+}
+
+int doubleInt(int a)
+{
+    if(a > MAX_INT)
+    {
+        return -1;
+    }
+    return a*2;
+}
+
